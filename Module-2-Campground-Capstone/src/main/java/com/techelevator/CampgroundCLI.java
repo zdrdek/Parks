3974e87b964265c1126f62c2ce7718071bfd8b1a
Kevin Sohngen <sohngen.5@osu.edu>
2018-06-27 14:57:50 -0400

    asdf

--- Module-2-Campground-Capstone/src/main/java/com/techelevator/CampgroundCLI.java
+++ Module-2-Campground-Capstone/src/main/java/com/techelevator/CampgroundCLI.java
@@ -17,7 +17,17 @@ public class CampgroundCLI {
 	private static final String[] PARKS_ALL_MENU_OPTIONS = new String[] {MAIN_MENU_OPTIONS_ACADIA, 
 																			 MAIN_MENU_OPTIONS_ARCHES,
 																			 MAIN_MENU_OPTIONS_CNVP,
-																			 MENU_OPTION_RETURN_TO_MAIN};
+																			 MAIN_MENU_OPTIONS_EXIT};
+	private static final String PARK_INFO_OPTIONS_VIEW = "View Campgrounds";
+	private static final String PARK_INFO_OPTIONS_SEARCH = "Search for Reservations";
+	private static final String PARK_INFO_OPTIONS_RETURN = "Return to Previous Screen";
+	private static final String[] INFO_ALL_MENU_OPTIONS = new String[] {PARK_INFO_OPTIONS_VIEW,
+																			PARK_INFO_OPTIONS_SEARCH,
+																			PARK_INFO_OPTIONS_RETURN};
+	private static final String CAMPGROUND_OPTIONS_SEARCH = "Search for Available Reservation";
+	private static final String CAMPGROUND_OPTIONS_RETURN = "Return to Previous Screen";
+	private static final String[] CAMPGROUND_ALL_MENU_OPTIONS = new String[] {CAMPGROUND_OPTIONS_SEARCH,
+																				CAMPGROUND_OPTIONS_RETURN};
 	
 	private Menu menu;
 	private ParkDAO parkDAO;
@@ -50,16 +60,40 @@ public class CampgroundCLI {
 					           "Select a Park for further details:");
 			String choice = (String)menu.getChoiceFromOptions(PARKS_ALL_MENU_OPTIONS);
 			if(choice.equals(MAIN_MENU_OPTIONS_ACADIA)) {
-				
+				//INSERT DISPLAY METHOD WITH ACADIA PARAMETER
 			}
 			if(choice.equals(MAIN_MENU_OPTIONS_ARCHES)) {
-				
+				//INSERT DISPLAY METHOD WITH ARCHES PARAMETER
 			}
 			if(choice.equals(MAIN_MENU_OPTIONS_CNVP)) {
-				
+				//INSERT DISPLAY METHOD WITH CNVP PARAMETER
 			}
 			if(choice.equals(MAIN_MENU_OPTIONS_EXIT)) {
-				
+				break;	//ENDS PROGRAM
+			}
+			while (true) {
+				System.out.println("\n" + "Select a Command");
+				choice = (String)menu.getChoiceFromOptions(INFO_ALL_MENU_OPTIONS);
+				if (choice.equals(PARK_INFO_OPTIONS_VIEW)) {
+					while (true) {
+						//INSERT DISPLAY FOR CAMPGROUNDS
+						System.out.println("\n" + "Select a Command");
+						choice = (String)menu.getChoiceFromOptions(CAMPGROUND_ALL_MENU_OPTIONS);
+						if (choice.equals(CAMPGROUND_OPTIONS_SEARCH)) {
+							//move into search for campground reservation
+						}
+						if (choice.equals(CAMPGROUND_OPTIONS_RETURN)) {
+							break;
+						}
+					}
+					continue;
+				}
+				if (choice.equals(PARK_INFO_OPTIONS_SEARCH)) {
+					//move into search for campground reservation
+				}
+				if (choice.equals(PARK_INFO_OPTIONS_RETURN)) {
+					break;
+				}
 			}
 		}
 	}
