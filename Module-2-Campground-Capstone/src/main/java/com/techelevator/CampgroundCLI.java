b31f3dac301453855b6da006339251e5600c426f
Zack Drdek <zdrdek@gmail.com>
2018-06-27 13:36:57 -0400

    stuff

--- Module-2-Campground-Capstone/src/main/java/com/techelevator/CampgroundCLI.java
+++ Module-2-Campground-Capstone/src/main/java/com/techelevator/CampgroundCLI.java
@@ -5,6 +5,13 @@ import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class CampgroundCLI {
+	
+	private static final String MAIN_MENU_OPTIONS_ACADIA = "Acadia";
+	private static final String MAIN_MENU_OPTIONS_ARCHES = "Arches";
+	private static final String MAIN_MENU_OPTIONS_CNVP = "Cuyahoga National Valley Park";
+	private static final String MAIN_MENU_OPTIONS_EXIT = "Exit";
+	private static final String MENU_OPTION_RETURN_TO_MAIN = "Return to main menu";
+	
 
 	public static void main(String[] args) {
 		BasicDataSource dataSource = new BasicDataSource();
