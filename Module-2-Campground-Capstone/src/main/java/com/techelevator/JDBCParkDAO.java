8c9027200ed18e795839dbd061c959f2e26cf572
Kevin Sohngen <sohngen.5@osu.edu>
2018-06-27 14:15:06 -0400

    non CLI changes

--- Module-2-Campground-Capstone/src/main/java/com/techelevator/JDBCParkDAO.java
+++ Module-2-Campground-Capstone/src/main/java/com/techelevator/JDBCParkDAO.java
@@ -1,5 +1,33 @@
 package com.techelevator;
 
-public class JDBCParkDAO {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+public class JDBCParkDAO implements ParkDAO {
+	
+	private JdbcTemplate jdbcTemplate;
+
+	public JDBCParkDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	@Override
+	public List<Park> getAvailableParks() {
+		List<Park> parkList = new ArrayList<>();
+		String findAllParks = "SELECT * FROM park";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(findAllParks);
+		while (results.next()) {
+			Park thePark = new Park();
+			parkList.add(thePark);
+		}
+		return parkList;
+	}
+	
+	
 
 }
--- Module-2-Campground-Capstone/src/main/java/com/techelevator/ParkDAO.java
+++ Module-2-Campground-Capstone/src/main/java/com/techelevator/ParkDAO.java
@@ -1,5 +1,10 @@
 package com.techelevator;
 
+import java.util.List;
+
 public interface ParkDAO {
 
+	public List<Park> getAvailableParks();
+	
+	
 }
